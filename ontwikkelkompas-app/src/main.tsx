5901d826099ccae41f41e5a9ededd5df7bfdc27e
paulveth <paul@paulveth.com>
2025-08-09 00:56:09 +0200

    Delete ontwikkelkompas-app directory

--- ontwikkelkompas-app/src/main.tsx
+++
@@ -1,6 +0,0 @@
-// aanpassing voor PWA-registratie onderaan toevoegen
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
-  });
-}
